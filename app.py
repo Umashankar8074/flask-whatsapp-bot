8d66a04877ef71086d9a5ead985c71ad64b0fbb6
Umashankar8074 <bashaboyinaumashankar@gmail.com>
2023-12-28 09:22:22 +0530

    removing comment

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from twilio.twiml.messaging_response import MessagingResponse
 
-# sample code for sending response to our incoming whatsapp message
 
 from flask_cors import CORS
 
